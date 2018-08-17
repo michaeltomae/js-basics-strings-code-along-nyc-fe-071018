4c14ad329428f73fb191ab9c2aee14e80750b0f0
Michael Tomae <michael.tomae@gmail.com>
2018-08-17 14:21:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Grace Hopper';
 let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
-currentUser ='GRACE HOPPER';
 let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser + '!'
