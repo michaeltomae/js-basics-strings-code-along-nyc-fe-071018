9e9793ea7adcdf78c244d11c4338275580900de4
Michael Tomae <michael.tomae@gmail.com>
2018-08-17 14:26:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
 const currentUser = 'Grace Hopper';
-let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
-let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser + '!'
-let shortGreeting = 'Welcome, ' + currentUser + '!'
+const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!'
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser + '!'
+const shortGreeting = 'Welcome, ' + currentUser + '!'
